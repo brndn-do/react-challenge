e0f841494196352f492efc4e261f847d7b034125
brndn-do <dobrandon05@gmail.com>
2025-10-08 21:40:36 -0500

    select courses

--- src/components/CourseList.tsx
+++ src/components/CourseList.tsx
@@ -1,3 +1,5 @@
+import { useCallback, useState } from 'react';
+
 interface Course {
   term: string;
   number: string;
@@ -11,12 +13,31 @@ interface CourseListProps {
 }
 
 const CourseList = ({ courses, term }: CourseListProps) => {
+  const [selectedCourses, setSelectedCourses] = useState<Set<string>>(
+    new Set()
+  );
+
+  const toggleSelected = useCallback((courseNumber: string) => {
+    setSelectedCourses((prev) => {
+      const next = new Set(prev); // create new Set to change reference
+      if (next.has(courseNumber)) next.delete(courseNumber);
+      else next.add(courseNumber);
+      return next;
+    });
+  }, []);
+
   const filteredCourses = Object.values(courses).filter((c) => c.term === term);
+
   return (
     <ul>
       {filteredCourses.map((course) => {
+        const isSelected = selectedCourses.has(course.number);
         return (
-          <li key={course.number}>
+          <li
+            key={course.number}
+            onClick={() => toggleSelected(course.number)}
+            className={`course-item ${isSelected ? 'selected' : ''}`}
+          >
             <h6 className="term-number">{`${course.term} CS ${course.number}`}</h6>
             <p className="title">{`${course.title}`}</p>
             <p className="meets">{`${course.meets}`}</p>
